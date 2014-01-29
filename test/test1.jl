596e95f8f2757f970d5259cdf5c96cd91b74684a
Sharmila Gopirajan Sivakumar <sharmila.gopirajan@gmail.com>
2014-01-29 22:48:22 +0530

    test script for mysql prepared statements data fetching

---
+++ test/test1.jl
@@ -0,0 +1,38 @@
+using DBI
+using MySQL
+
+db = connect(MySQL5, "localhost", "test", "test123", "test")
+
+sql = "CREATE TABLE users (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255))"
+
+#stmt = prepare(db, sql)
+#execute(stmt)
+
+try
+#
+#\]]\\\]';pstmt = prepare(db, sql)
+#    execute(stmt)
+end
+
+errcode(db)
+errstring(db)
+
+#rowid = lastinsertid(db)
+
+#stmt = prepare(db, "INSERT INTO users (name) VALUES ('Jeff Bezanson')")
+#execute(stmt)
+
+#stmt = prepare(db, "INSERT INTO users (name) VALUES ('Viral Shah')")
+#execute(stmt)
+
+#stmt = prepare(db, "INSERT INTO users (name) VALUES ('Stefan Karpinski')")
+#execute(stmt)
+
+#rowid = lastinsertid(db)
+new_stmt = prepare(db, "SELECT * from users")
+execute(new_stmt)
+println(fetchrow(new_stmt))
+#stmt = prepare(db, "DROP TABLE users")
+#execute(stmt)
+
+disconnect(db)
